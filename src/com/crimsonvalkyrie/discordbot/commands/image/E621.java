c1e4b5a92e49b545a3eb0df5387be1c4bf62d24a
null_Zer0 <nullZer0@crimsonvalkyrie.com>
2019-05-18 14:15:39 +1000

    Added the ability to request up to 5 images per use of the e621 command

--- src/com/crimsonvalkyrie/discordbot/commands/image/E621.java
+++ src/com/crimsonvalkyrie/discordbot/commands/image/E621.java
@@ -1,43 +1,109 @@
 package com.crimsonvalkyrie.discordbot.commands.image;
 
+import com.crimsonvalkyrie.discordbot.main.Main;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import kong.unirest.GetRequest;
 import kong.unirest.HeaderNames;
 import kong.unirest.Unirest;
-import org.json.JSONPointerException;
+import net.dv8tion.jda.core.entities.ChannelType;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
 
 public class E621 extends Command
 {
-	final GetRequest request = Unirest.get("https://e621.net/post/index.json")
-			.header(HeaderNames.USER_AGENT, "FurryBoy/1.0 (admin@crimsonvalkyrie.com)")
-			.queryString("limit", 1);
+	Logger logger = Main.getLogger();
+
+	final GetRequest getRequest = Unirest.get("https://e621.net/post/index.json")
+			.header(HeaderNames.USER_AGENT, "FurryBoy/1.1 (admin@crimsonvalkyrie.com)");
 
 	public E621()
 	{
 		name = "e621";
-		arguments = "[tags]";
+		arguments = "[c=count] [tag1 tag2...]";
 		guildOnly = false;
+		cooldown = 2;
 
 
-		help = "gets a single image from e621";
+		help = "gets images from e621. Limit=5 per message";
 	}
 
 	protected void execute(CommandEvent event)
 	{
-		String[] arguments = event.getArgs().split(" ");
+		if(event.isFromType(ChannelType.PRIVATE) || event.getTextChannel().isNSFW())
+		{
+			GetRequest request;
+			String[] arguments;
+			String tags = "";
+			int count = 1;
+			JSONArray response;
+			StringBuilder reply;
 
-		try
-		{
-			event.reply("" + request.queryString("tags", "order:random " + event.getArgs())
-					.asJson().getBody().getArray().query("/0/file_url"));
-		}
-		catch(JSONPointerException e)
-		{
-			if(e.getMessage().equals("index 0 is out of bounds - the array has 0 elements"))
+			logger.info("{} requested some nudes\nArguments: {}", event.getAuthor(), event.getArgs());
+			arguments = event.getArgs().split("(?<=(c=\\d)) ");
+			logger.info("Arguments split into {} arguments", arguments.length);
+
+			if(arguments.length == 1 && arguments[0].startsWith("c=") && arguments[0].contains(" "))
 			{
-				event.reply("No Results");
+				logger.info("Invalid argument: {}", arguments[0]);
+				reply = new StringBuilder("Invalid Arguments");
 			}
+			else
+			{
+				reply = new StringBuilder();
+
+				if(arguments.length > 1)
+				{
+					tags = arguments[1];
+				}
+				if(arguments[0].contains("c="))
+				{
+					try
+					{
+						count = Integer.valueOf(arguments[0].replace("c=", ""));
+						if(count > 5)
+						{
+							reply.append("Limit is 5, but ").append(count).append(" was requested, changing to 5");
+							count = 5;
+						}
+					}
+					catch(NumberFormatException e)
+					{
+						reply.append("\n").append("Invalid count given, defaulting to 1");
+					}
+				}
+				else
+				{
+					tags = arguments[0];
+				}
+
+				request = getRequest.queryString("limit", count);
+				response = request.queryString("tags", "order:random " + tags).asJson().getBody().getArray();
+
+				if(response.length() > 0)
+				{
+					if(response.length() < count)
+					{
+						logger.info("Given count was higher than amount returned");
+						reply.append("\n").append("Requested ").append(count).append(", but only got ").append(response.length()).append(" results");
+						count = response.length();
+					}
+					for(int i = 0; i < count; i++)
+					{
+						reply.append("\n").append(response.query("/" + i + "/file_url"));
+					}
+				}
+				else
+				{
+					logger.info("No results found");
+					reply.append("No results");
+				}
+			}
+			event.reply(reply.toString());
+		}
+		else
+		{
+			event.reply("This is an NSFW only command");
 		}
 	}
 }
